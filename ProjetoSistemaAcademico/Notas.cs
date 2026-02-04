f38fbef0fdb0d81506669f2ac3b205b50abfdf99
JOÃO PEDRO SILVA DE ALMEIDA <232.975651@universidadesenaicimatec.edu.br>
2026-02-04 15:39:29 -0300

    Update

---
+++ ProjetoSistemaAcademico/Notas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoSistemaAcademico
+{
+    public class Notas : Aluno
+    {
+        float Matematica { get; set; }
+        float Portugues { get; set; }
+        float Ciencias { get; set; }
+        float Historia { get; set; }
+        float Geografia { get; set; }
+    public Notas(int numMatricula, float matematica, float portugues, float ciencias, float historia, float geografia)
+        {
+            NumMatricula = numMatricula;
+            Matematica = matematica;
+            Portugues = portugues;
+            Ciencias = ciencias;
+            Historia = historia;
+            Geografia = geografia;
+
+        }
+        public override string ToString()
+        {
+            return
+                "===============================\n" +
+                $"Número de Matrícula: {NumMatricula}\n" +
+                "===============================\n" +
+                $"Nota de Matemática: {Matematica}\n" +
+                $"Nota de Português: {Portugues}\n" +
+                $"Nota de Ciências: {Ciencias}\n" +
+                $"Nota de História: {Historia}\n" +
+                $"Nota de Geografia: {Geografia}\n" +
+                "===============================\n";
+        }
+}
---
+++ ProjetoSistemaAcademico/Professor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoSistemaAcademico
+{
+    public class Professor : Pessoa
+    {
+        float Salario { get; set; }
+
+        public Professor(string nome, long cpf, DateTime dataNascimento, float salario)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            DataNascimento = dataNascimento;
+            Salario = salario;
+        }
+
+        public override string ToString()
+        {
+
+            return
+                "===============================\n" +
+                $"Nome: {Nome}\n" +
+                $"CPF: {Cpf}\n" +
+                $"Data de Nascimento: {DataNascimento.ToShortDateString()}\n" +
+                $"Sálario: {Salario}\n" +
+                "===============================\n";
+
+
+        }
+    }
+
+}
