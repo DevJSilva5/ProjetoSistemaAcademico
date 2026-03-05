f5420f8c1f131c166910cb15e4c23f3ea0a14cf9
JOÃO PEDRO SILVA DE ALMEIDA <232.975651@universidadesenaicimatec.edu.br>
2026-03-05 14:28:27 -0300

    Update: Organização de Pastas, Interface Autenticavel

---
+++ ProjetoSistemaAcademico/Models/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoSistemaAcademico
+{
+    // A classe Pessoa representa uma pessoa genérica no sistema acadêmico, contendo informações básicas como nome, CPF e data de nascimento
+    public class Pessoa
+    {
+        // Propriedade para o nome da pessoa
+        public string Nome { get; set; }
+        // Propriedade para o CPF da pessoa
+        public string Cpf { get; set; }
+        // Propriedade para a data de nascimento da pessoa
+        public DateTime DataNascimento { get; set; }
+    }
+}
+
